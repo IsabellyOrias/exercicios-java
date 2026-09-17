5516e52202cc3f7292055309ed255acc08b2f3c0
IsabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-17 12:49:23 -0300

    Realização exercicio 11

---
+++ parte3_repeticaoWhile/ex11.java
@@ -0,0 +1,11 @@
+package parte3_repeticaoWhile;
+
+public class ex11 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <=10) {
+        System.out.println(i);
+        i++;
+        }
+    }
+}
