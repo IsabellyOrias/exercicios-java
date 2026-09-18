357d321780c16f74e47cd111a4ef9a2abebbc8ec
IsabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-18 09:07:13 -0300

    Realização exercicio 18

---
+++ parte4_repeticaoFor/ex18.java
@@ -0,0 +1,12 @@
+package parte4_repeticaoFor;
+
+public class ex18 {
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int numero = 1; numero <= 100; numero++) {
+            soma += numero;
+        }
+        System.out.println("A soma dos números 1 até 100 é: " + soma);
+    }
+    
+}
