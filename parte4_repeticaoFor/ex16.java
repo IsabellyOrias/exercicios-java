7e56d70b4a19b30db9a9cf020713379fd1d190df
IsabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-18 08:58:39 -0300

    Realização exercicio 16

---
+++ parte4_repeticaoFor/ex16.java
@@ -0,0 +1,11 @@
+package parte4_repeticaoFor;
+
+public class ex16 {
+    public static void main(String[] args) {
+        
+        for ( int numero = 1; numero <= 20; numero++) {
+            System.out.println(numero);
+        } 
+    }
+    
+}
