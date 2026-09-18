34b5928a96720cdc2cac0ccfc22946c0cc93247c
IsabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-18 09:19:23 -0300

    Realização exercicio 20

---
+++ parte4_repeticaoFor/ex20.java
@@ -0,0 +1,11 @@
+package parte4_repeticaoFor;
+
+public class ex20 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i --) {
+            System.out.println(i);
+        }
+          System.out.println("Fim");
+    }
+    
+}
