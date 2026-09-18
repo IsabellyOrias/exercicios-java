635275e8ce835f7c628e1d75131a255dc2ce1e1b
IsabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-18 09:02:28 -0300

    Realização exercicio 17

---
+++ parte4_repeticaoFor/ex17.java
@@ -0,0 +1,10 @@
+package parte4_repeticaoFor;
+
+public class ex17 {
+    public static void main(String[] args) {
+        for (int numero = 2; numero <= 20; numero +=2) {
+            System.out.println(numero);
+        };
+    }
+    
+}
