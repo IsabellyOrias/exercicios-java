87c3a82fcfb93cbdc206e6c6f61f06a2e272de3b
isabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-22 16:16:38 -0300

    Realização exercicio 25

---
+++ parte5_array/ex25.java
@@ -0,0 +1,11 @@
+package parte5_array;
+
+public class ex25 {
+    public static void main(String[] args) {
+        int[] lista = { 3, 7, 1, 9, 4 };
+
+        for (int i = lista.length - 1; i >= 0; i--) {
+            System.out.println(lista[i]);
+        }
+    }
+}
