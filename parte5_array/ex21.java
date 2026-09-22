f0fe4e492c12cee5c6beea1c927266c625b91fe7
isabellyOrias <isabelly_orias@estudante.sesisenai.org.br>
2026-09-22 16:07:14 -0300

    Realização exercicio 21

---
+++ parte5_array/ex21.java
@@ -0,0 +1,11 @@
+package parte5_array;
+
+public class ex21 {
+    public static void main(String[] args) {
+        int[] lista = {1, 2, 6, 4, 5};
+
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println(lista[i]);
+        }
+    }
+}
